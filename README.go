f8cd14e18693862c7cca1da3354b191fb768e7e9
Bryan Reynaert <br@akori.cl>
2024-08-30 23:42:35 -0700

    Add readme with some renders

---
+++ README.go
@@ -0,0 +1,12 @@
+Hello traveller. This repo contains my exploration of building a 3D renderer based off `The Ray Tracer Challenge` by Jamis Buck, a fun read.
+It is not intended to be optimal, smart, clean, etc... just my ramblings as I learn something new, something old and something stolen. If
+this is something that interests you, I invite you to follow along the commit history, it might save you having to do some typing (although
+research has found it is essential for learning)
+
+To show off the capabilities of the renderer, see this image which is the result of chapter 5:
+
+![Basic rendering of a Sphere](/examples/chapter5/example.png?raw=true "BasicSphereRender")
+
+Then, in chapter 6 we added light sources and their consequences according to the Phong model:
+
+![Illuminated rendering of a Sphere](/examples/chapter6/example.png?raw=true "IlluminatedSphereRender")
