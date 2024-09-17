280bc61c4d7c17d1cf00204a98d14aa0417d5c7f
Bryan Reynaert <br@akori.cl>
2024-09-17 00:40:00 -0700

    Add profiling information

---
+++ go/examples/profiling/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"raytracerchallenge/render"
+	"runtime/pprof"
+	"time"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
+func main() {
+	flag.Parse()
+
+	from := render.NewPoint(0, 2.5, -5)
+	to := render.NewPoint(0, 0.5, 0)
+	up := render.NewVector(0, 1, 0)
+	camera := render.NewCamera(1000, 500, math.Pi/3)
+	camera.SetTransform(render.View(from, to, up))
+	camera.SetParallelism(32)
+	camera.SetAntialiasing(3)
+
+	world := render.NewWorld()
+	l1 := render.NewPointLight(render.NewPoint(-10, 10, -10), render.NewColor(1, 1, 1))
+	world.AddLight(l1)
+
+	patternCheckers := render.NewCheckersPattern(render.NewColor(1, 1, 1), render.NewColor(0.5, 0.5, 0.5))
+
+	floor := render.NewPlane()
+	floor.SetTransform(render.Translation(0, -0.1, 0))
+	floorMaterial := render.NewMaterial(render.NewColor(1, 0.9, 0.9), 0.1, 0.9, 0, 0, 0, 1, 200, patternCheckers)
+	floor.SetMaterial(floorMaterial)
+	world.AddObject(floor)
+
+	greenGlass := render.NewMaterial(render.NewColor(0, 0.3, 0), 0.1, 0.1, 0.8, 0, 0.8, 2, 200, nil)
+	c1 := render.NewCube()
+	c1.SetMaterial(greenGlass)
+	c2 := render.NewCube()
+	c2.SetMaterial(greenGlass)
+	c2.SetTransform(render.Translation(0.5, 0.5, 0.1))
+
+	g := render.NewGroup()
+	g.Add(c1)
+	g.Add(c2)
+	g.SetTransform(render.Translation(-1.5, 0, 0))
+	world.AddObject(g)
+
+	c3 := render.NewCube()
+	c3.SetMaterial(greenGlass)
+	c4 := render.NewCube()
+	c4.SetMaterial(greenGlass)
+	c4.SetTransform(render.Translation(0.5, 0.5, 0.1))
+	csg := render.NewCSGShape(render.CSG_UNION, c3, c4)
+	csg.SetTransform(render.Translation(1.5, 0, 0))
+	world.AddObject(csg)
+
+	progress := make(chan bool)
+	go func(progress chan bool) {
+		count := 0
+		fmt.Println(count, "/ 10")
+		for <-progress {
+			count++
+			fmt.Println(count, "/ 10")
+		}
+	}(progress)
+
+	startTime := time.Now()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+	}
+	image := camera.Render(world, progress)
+	if *cpuprofile != "" {
+		pprof.StopCPUProfile()
+	}
+
+	duration := time.Since(startTime)
+
+	fmt.Println("Rendered in", duration)
+
+	f, err := os.Create("example.png")
+	if err != nil {
+		panic(err)
+	}
+	image.Encode(f)
+}
